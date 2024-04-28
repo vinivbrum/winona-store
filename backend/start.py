3bd3266d390543dd0d7292ef3749dd781f977c17
Vinivbrum <vinivbrum@gmail.com>
2024-04-28 00:27:41 -0300

    alterar o start

--- backend/start.py
+++ backend/start.py
@@ -1,7 +1,8 @@
 from datasource.api import APICollector
+from contracts.schema import ProdutoSchema, GenericSchema
 
-
-
-minha_classe = APICollector().getData()
+schema = ProdutoSchema
+sku = 1001
+minha_classe = APICollector().start(sku)
 print (minha_classe)
 
