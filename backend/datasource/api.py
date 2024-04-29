a70047e52bf06024178e11d0ff3b08f55e9f56e8
Vinivbrum <vinivbrum@gmail.com>
2024-04-29 20:09:07 -0300

    alteração da extração de dados pela api

--- backend/datasource/api.py
+++ backend/datasource/api.py
@@ -1,14 +1,11 @@
 from datasource.user.credenciais import USER_EMAIL, ACESS_TOKEN, CLIENT_ID
-from contracts.schema import ProdutoSchema, VariantSchema, CategorySchema
 import requests
-import json
-import pydantic
 from typing import List, Any, Dict, Union
 from datetime import datetime
 
 class APICollector:
-    def __init__(self):
-        self._schema = None
+    def __init__(self,schema):
+        self._schema = schema
         self._render = None
         self._buffer = None
         return
@@ -19,7 +16,7 @@ class APICollector:
         return (resp)
     
     def getData(self,param):
-        url = f'https://api.tiendanube.com/v1/{CLIENT_ID}/products/sku/{param}'
+        url = f'https://api.tiendanube.com/v1/{CLIENT_ID}/products?{param}'
         headers = {'Authentication': f'bearer {ACESS_TOKEN}',
                    'User-Agent': 'winona-api ({USER_EMAIL})'
                 }
@@ -29,32 +26,29 @@ class APICollector:
         except requests.exceptions.HTTPError as err:
             print("Error making request", err)
             return None
-    @staticmethod
-    def extractData(response: Dict[str, Any]) -> Dict[str, Any]:
-        extracted_data: Dict[str, Any] = {}
-        # Função para extrair dados de acordo com o schema
-        def extract_with_schema(data: Dict[str, Any], schema: Dict[str, Any]) -> Dict[str, Any]:
-            extracted: Dict[str, Any] = {}
-            for key, value_type in schema.items():
-                if key in data:
-                    if isinstance(value_type, dict):
-                        extracted[key] = extract_with_schema(data[key], value_type)
-                    elif value_type == datetime:
-                        extracted[key] = datetime.strptime(data[key], "%Y-%m-%dT%H:%M:%S%z")
-                    else:
-                        extracted[key] = data[key]
-            return extracted
-
-        # Extrair informações usando o schema do produto
-        extracted_data = extract_with_schema(response, ProdutoSchema)
-
-        # Extrair variantes
-        extracted_data['variants'] = [extract_with_schema(variant_data, VariantSchema) for variant_data in response['variants']]
-
-        # Extrair categorias
-        extracted_data['categories'] = [extract_with_schema(category_data, CategorySchema) for category_data in response['categories']]
-
+        
+    def extractData(self, response: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        extracted_data = []
+        for product in response:
+            product_data = self.extract_product_data(product)
+            extracted_data.append(product_data)
         return extracted_data
+
+    def extract_product_data(self, product: Dict[str, Any]) -> Dict[str, Any]:
+        extracted_product_data = self.extract_with_schema(product, self._schema)
+        extracted_product_data['variants'] = [self.extract_with_schema(variant, self._schema['variants']) for variant in product['variants']]
+        return extracted_product_data
+
+    def extract_with_schema(self, data: Dict[str, Any], schema: Dict[str, Any]) -> Dict[str, Any]:
+        extracted = {}
+        for key, value_type in schema.items():
+            if key in data:
+                if isinstance(value_type, dict):
+                    extracted[key] = self.extract_with_schema(data[key], value_type)
+                else:
+                    extracted[key] = data[key]
+        return extracted
+
     
     
     def transform_data(self):
