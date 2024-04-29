1594d007f0fa51f8b834b76c5948a1407fcc977a
Vinivbrum <vinivbrum@gmail.com>
2024-04-29 20:08:39 -0300

    alteração dos chemas padrões

--- backend/contracts/schema.py
+++ backend/contracts/schema.py
@@ -1,33 +1,16 @@
-from typing import Union, Dict, List
+from typing import Union, Dict, List, Any
 from datetime import datetime
 
 GenericSchema = Dict[str, Union[int, str, bool, None]]
 
-VariantSchema: GenericSchema = {
-    'id': int,
-    'sku': str,
-    'price': str,
-    'created_at': datetime,
-    'updated_at': datetime,
-}
-
-CategorySchema: GenericSchema = {
-    'id': int,
-    'name': str,
-    'description': str,
-    'handle': str,
-    'parent': Union[int, None],
-    'subcategories': List[int],
-    'seo_title': str,
-    'seo_description': str,
-    'google_shopping_category': str,
-    'created_at': datetime,
-    'updated_at': datetime,
-}
-
-ProdutoSchema: GenericSchema = {
+ProdutoSchema = {
     'id': int,
     'name': Dict[str, str],
-    'variants': List[VariantSchema],
-    'categories': List[CategorySchema],
+    'handle': Dict[str, str],
+    'variants': {
+        'id': int,
+        'value': str,
+        'sku': str,
+        'price': str
+    }
 }
