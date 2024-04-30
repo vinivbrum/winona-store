1bc0ecdab2985dd2799ec5f54be544388a9970bd
Vinivbrum <vinivbrum@gmail.com>
2024-04-30 17:53:15 -0300

    alterar start

--- backend/start.py
+++ backend/start.py
@@ -1,8 +1,8 @@
 from datasource.api import APICollector
 from contracts.schema import ProdutoSchema, GenericSchema
 
+
 schema = ProdutoSchema
-sku = 'fields=id,name,handle,variants'
+sku = 'page=1&per_page=190&fields=id,name,handle,variants'
 minha_classe = APICollector(schema).start(sku)
 print (minha_classe)
-
